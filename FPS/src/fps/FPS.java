0a0c69be83322b0a295fc156b6cbf06e92c5dbff
goodofd3ath2 <leticiaposchi@gmail.com>
2015-10-28 01:44:43 -0200

    <3

--- FPS/src/fps/FPS.java
+++ FPS/src/fps/FPS.java
@@ -30,9 +30,16 @@ public class FPS {
         JOptionPane.showMessageDialog(null, perso.resumo());
         
         Armas arma = new Armas();
+        arma.setDano(22.2);
+        arma.setPente(7);
+        arma.setPeso(1.2);
         JOptionPane.showMessageDialog(null, arma.resumo());
         
         Shield shi = new Shield();
+        shi.setCor("azul");
+        shi.setMaterial("Metal");
+        shi.setPeso(12);
+        shi.setTamanho(1.2);
         JOptionPane.showMessageDialog(null, shi.resumo());
     }
     
